a2293defebdf5657b61395182704fb6d12aceee8
hmsmiraz <hmsmiraz64729@gmail.com>
2023-09-27 01:40:23 +0600

    Add show all button in all donation cart

--- src/Components/Donations/Donations.jsx
+++ src/Components/Donations/Donations.jsx
@@ -1,17 +1,19 @@
+
 import DonationCards from "./DonationCards";
 
 const Donations = ({ donations }) => {
-    
-    return (
-        <div className="mt-5 py-10">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-                {
-                   donations?.map(donation=><DonationCards key={donation.id} 
-                   donation={donation}></DonationCards>) 
-                }
-            </div>
-        </div>
-    );
+
+  return (
+    <div className="mt-5 py-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+        {donations?.map((donation) => (
+          <DonationCards key={donation.id} donation={donation}></DonationCards>
+        ))}
+      </div>
+
+
+    </div>
+  );
 };
 
 export default Donations;
--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -5,6 +5,9 @@ const Donation = () => {
   const [donations, setDonations] = useState([]);
   const [noDonations, setNoDonations] = useState(false);
 
+  const [dataLength, setDataLength] = useState(4);
+  console.log(donations.length);
+
   useEffect(() => {
     const donationsItems = JSON.parse(localStorage.getItem("donations"));
     if (donationsItems) {
@@ -16,16 +19,29 @@ const Donation = () => {
   return (
     <div>
       {noDonations ? (
-        <p className="h-[80vh] flex justify-center items-center">{noDonations}</p>
+        <p className="h-[80vh] flex justify-center items-center">
+          {noDonations}
+        </p>
       ) : (
         <div>
-            <div className="grid grid-col-1 lg:grid-cols-2 gap-5 mt-5">
-                {
-                    donations?.map((donation)=><DonationAllCard
-                    key={donation.id}
-                    donation={donation}></DonationAllCard>)
-                }
+          <div className="grid grid-col-1 lg:grid-cols-2 gap-5 mt-5">
+            {donations?.slice(0, dataLength).map((donation) => (
+              <DonationAllCard
+                key={donation.id}
+                donation={donation}
+              ></DonationAllCard>
+            ))}
+          </div>
+          <div className={dataLength === donations.length ? "hidden" : ""}>
+            <div className="flex items-center justify-center my-5">
+              <button
+                onClick={() => setDataLength(donations.length)}
+                className="btn btn-primary px-6"
+              >
+                Show All
+              </button>
             </div>
+          </div>
         </div>
       )}
     </div>
